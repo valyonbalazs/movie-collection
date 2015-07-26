f20a6d3d3edf20435d16940c86e365d5419dea34
Balazs Valyon <valyon.balazs@gmail.com>
2015-07-27 00:22:42 +0200

    get the max voted movie (max number of votes)

--- client/js/moviemodel.js
+++ client/js/moviemodel.js
@@ -37,3 +37,53 @@
 
 var movie = new Movie('star wars', 'overview', '7.2', '2005', '/gnavakvkla.jph', '/cvsacsa.jpg');
 */
+
+var moviesData = [];
+
+var movies = {
+  getMovie: function (movieName) {
+    var movieData;
+    var xmlhttp = new XMLHttpRequest();
+    var api_key = "&api_key=4a8dce0b18b88827ffbc32dee5b66838";
+    var urlFirstPart = "https://api.themoviedb.org/3/search/movie?query=";
+    var url = urlFirstPart + movieName + api_key;
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+         if(xmlhttp.status == 200){
+             movieData = JSON.parse(xmlhttp.responseText);
+             var bestVoted = movies.getMaxVotedElement(movieData, movieName);
+         }
+         else if(xmlhttp.status == 400) {
+            console.log("ERROR: status message 400");
+         }
+         else {
+             console.log("ERROR: status is: " + xmlhttp.status);
+         }
+      }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  },
+  getMaxVotedElement: function (moviesResult, movieTitle) {
+    var result = moviesResult.results;
+    console.log(movieTitle);
+    var maxVoted;
+    var voteCount = 0;
+    for(var key in result) {
+      var item = result[key];
+      //console.log(item);
+      if((item !== null) && (item.hasOwnProperty("vote_count"))) {
+        if(parseInt(item.vote_count) > voteCount) {
+          voteCount = item.vote_count;
+          maxVoted = item;
+        }
+      }
+    }
+    return maxVoted;
+  }
+};
+
+(function() {
+  movies.getMovie("star wars episode iv");
+}());
